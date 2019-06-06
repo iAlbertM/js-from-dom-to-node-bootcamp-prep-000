82ab0b138c487a4c40cbddf43c04afffc1f42865
Albert Martinez <albertwdd@gmail.com>
2019-06-06 22:41:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
 // write your code below
-console.log("Hello, World!");    760 778 2305
-
-DAP
-3:40pm
-Palm Springs Medical Center
-
-1515 N Sunrise Way
-through back 
+console.log("Hello, World!");
